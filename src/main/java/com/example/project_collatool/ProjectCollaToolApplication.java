21aa0ea350eb92e18290e7606e3a955362fd4b7f
inn-jeong <jinpal429@gmail.com>
2023-07-25 22:22:05 +0900

    Initial commit

---
+++ src/main/java/com/example/project_collatool/ProjectCollaToolApplication.java
@@ -0,0 +1,13 @@
+package com.example.project_collatool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCollaToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectCollaToolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/project_collatool/ProjectCollaToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project_collatool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCollaToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
